49959d20633b5e80b7a8f0a0b919b71fd56fce7d
Marten Schilstra <mail@martenschilstra.nl>
2016-06-24 15:26:48 -0400

    Initial implementation

---
+++ service-worker/ember-service-worker-cache-first.js
@@ -0,0 +1,10 @@
+self.addFetchListener(function(event) {
+  return caches.open('esw-cache-first').then(function(cache) {
+    return cache.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    });
+  });
+});
