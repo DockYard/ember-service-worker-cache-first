bba7aaa1a0b1c54d58eb1f47ea47ea7418685aba
simon <ihmig@kaliber5.de>
2017-03-20 17:35:20 +0100

    Fix escaping of URL regexs

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,17 +1,11 @@
 import { PATTERNS, VERSION } from 'ember-service-worker-cache-first/service-worker/config';
 import cleanupCaches from 'ember-service-worker/service-worker/cleanup-caches';
+import { createUrlRegEx, urlMatchesAnyPattern } from 'ember-service-worker/service-worker/url-utils';
 
 const CACHE_KEY_PREFIX = 'esw-cache-first';
 const CACHE_NAME = `${CACHE_KEY_PREFIX}-${VERSION}`;
 
-const PATTERN_REGEX = PATTERNS.map((pattern) => {
-  let normalized = new URL(pattern, self.location).toString();
-  return new RegExp(`^${normalized}$`);
-});
-
-const MATCH = (key) => {
-  return !!PATTERN_REGEX.find((pattern) => pattern.test(key));
-};
+const PATTERN_REGEX = PATTERNS.map(createUrlRegEx);
 
 self.addEventListener('fetch', (event) => {
   let request = event.request;
@@ -19,7 +13,7 @@ self.addEventListener('fetch', (event) => {
     return;
   }
 
-  if (MATCH(request.url)) {
+  if (urlMatchesAnyPattern(request.url, PATTERN_REGEX)) {
     event.respondWith(
       caches.open(CACHE_NAME).then((cache) => {
         return cache.match(request).then((response) => {
